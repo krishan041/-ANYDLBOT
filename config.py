72f983f0ce26eb9c71b23f58bcf34907d05f9966
manishadara76 <manishadara76@gmail.com>
2025-01-25 14:33:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
     SUPER7X_DLBOT_USERS = [6428531614]
+    SUPER3X_DLBOT_USERS = [6428531614]
+    BANNED_USERS = []
 
     # Telegram bot credentials from @BotFather
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "").strip()
